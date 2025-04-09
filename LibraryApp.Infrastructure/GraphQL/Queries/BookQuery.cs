7347f8228c58517823202643f91d8ade5df8d654
Lucas Oliveira <lucas.oliveira@multiclubes.com.br>
2025-04-09 17:24:57 -0300

    Create basic structure for GraphQL

---
+++ LibraryApp.Infrastructure/GraphQL/Queries/BookQuery.cs
@@ -0,0 +1,42 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using LibraryApp.Domain.Interfaces.Repositories;
+using LibraryApp.Infrastructure.GraphQL.Types;
+
+namespace LibraryApp.Infrastructure.GraphQL.Queries
+{
+	public class BookQuery : ObjectGraphType
+	{
+		public BookQuery(IBookRepository bookRepository)
+		{
+			Field<BookType>("books")
+				.Description("Lista de todos os livros")
+				.ResolveAsync(async context => await bookRepository.GetAllAsync()
+			);
+
+			Field<BookType>("book")
+				.Description("Busca o livro pelo ID")
+				.Argument<NonNullGraphType<IdGraphType>>("id", "ID do livro")
+				.ResolveAsync(async context => await bookRepository.GetByIdAsync(context.GetArgument<Guid>("id"))
+			);
+
+			Field<BookType>("bookByISBN")
+				.Description("Busca o livro pelo ISBN")
+				.Argument<NonNullGraphType<StringGraphType>>("isbn", "ISBN do livro")
+				.ResolveAsync(async context => await bookRepository.GetByISBNAsync(context.GetArgument<string>("isbn"))
+			);
+
+			Field<ListGraphType<BookType>>("booksByTitle")
+				.Description("Busca os livros pelo título")
+				.Argument<NonNullGraphType<StringGraphType>>("title", "Título do livro")
+				.ResolveAsync(async context => await bookRepository.GetByTitleAsync(context.GetArgument<string>("title"))
+			);
+
+			Field<ListGraphType<BookType>>("booksByAuthor")
+				.Description("Busca os livros pelo autor")
+				.Argument<NonNullGraphType<StringGraphType>>("author", "Autor do livro")
+				.ResolveAsync(async context => await bookRepository.GetByAuthorAsync(context.GetArgument<string>("author"))
+			);
+		}
+	}
+}
---
+++ LibraryApp.Infrastructure/GraphQL/Types/BookType.cs
@@ -0,0 +1,24 @@
+﻿using GraphQL.Types;
+using LibraryApp.Domain.Entities;
+
+namespace LibraryApp.Infrastructure.GraphQL.Types
+{
+	public class BookType : ObjectGraphType<Book>
+	{
+		public BookType()
+		{
+			Field(x => x.Id, type: typeof(IdGraphType)).Description("ID do Livro");
+			Field(x => x.Title).Description("Título do Livro");
+			Field(x => x.Author).Description("Autor do Livro");
+			Field(x => x.ISBN).Description("ISBN do livro");
+			Field(x => x.PublicationYear).Description("Ano de publicação");
+			Field(x => x.Publisher).Description("Editora");
+			Field(x => x.AvaliableCopies).Description("Exemplares disponíveis");
+			Field(x => x.TotalCopies).Description("Total de exemplares");
+			Field(x => x.Genre).Description("Gênero do livro");
+			Field(x => x.Description, nullable: true).Description("Descrição do livro");
+			Field(x => x.CreatedAt).Description("Data de criação");
+			Field(x => x.UpdatedAt).Description("Data da ultima atualização");
+		}
+	}
+}
